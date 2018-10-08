f91d75b961e8bbbe3bf30317e2c466e4da1d2199
Rashika Patel <rashika_patel@hotmail.com>
2018-10-08 17:37:01 +0100

    moved presentation logic from account class, and turned statement into hah

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,25 +1,24 @@
 require 'account'
 describe Account do
   subject(:account) { described_class.new }
-  it 'should return initial balance of 0' do
-    expect(account.balance).to eq 0
-  end
 
   it 'should increase balance when deposit is made' do
     expect(account).to respond_to(:deposit).with(1).argument
+    expect(account.balance).to eq 0
     account.deposit(1000)
     expect(account.balance).to eq 1000
   end
 
   it 'should decrease balance when withdrawal is made' do
     expect(account).to respond_to(:withdrawal).with(1).argument
+    expect(account.balance).to eq 0
     account.withdrawal(500)
     expect(account.balance).to eq(-500)
   end
 
   it 'should store transaction row' do
     account.deposit(1000)
-    expect(account.statement).to include(" || 1000.00 || || 1000.00")
+    expect(account.statement[0]).to include(amount: 1000, balance: 1000)
   end
 
   it 'should print statement' do
