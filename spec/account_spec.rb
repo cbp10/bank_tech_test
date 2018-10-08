f584870fb9fd42042ca833454667dec259080c2d
Rashika Patel <rashika_patel@hotmail.com>
2018-10-08 10:57:20 +0100

    can access account balance, initially 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,4 +1,7 @@
 require 'account'
 describe Account do
-  
+  subject(:account) { described_class.new }
+  it 'should return initial balance of 0' do
+    expect(account.balance).to eq 0
+  end
 end
